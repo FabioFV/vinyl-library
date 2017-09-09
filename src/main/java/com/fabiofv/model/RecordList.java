87379716954e09ca19b6905309f78bc42a20765f
Fabio Ferreras <fabio_ferreras@hotmail.com>
2017-09-08 22:17:01 -0400

    Add missing getters and setters

--- src/main/java/com/fabiofv/model/RecordList.java
+++ src/main/java/com/fabiofv/model/RecordList.java
@@ -23,6 +23,16 @@ public class RecordList
     @JoinTable(name = "LIST_RECORD")
     private List<Record> list;
 
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+
     public String getName()
     {
         return name;
--- src/main/java/com/fabiofv/model/Song.java
+++ src/main/java/com/fabiofv/model/Song.java
@@ -17,4 +17,44 @@ public class Song
 
     private int minutes;
     private int seconds;
+
+    public long getId()
+    {
+        return id;
+    }
+
+    public void setId(long id)
+    {
+        this.id = id;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public int getMinutes()
+    {
+        return minutes;
+    }
+
+    public void setMinutes(int minutes)
+    {
+        this.minutes = minutes;
+    }
+
+    public int getSeconds()
+    {
+        return seconds;
+    }
+
+    public void setSeconds(int seconds)
+    {
+        this.seconds = seconds;
+    }
 }
